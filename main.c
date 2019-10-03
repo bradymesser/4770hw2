de4415da6b2f5b5f8a0b025a3fcd15cb85c74318
William B Messer <wmesser@clemson.edu>
2019-10-03 11:25:56 -0400

    MAKE GIT

---
+++ main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+// function prototypes
+void CopyNewToOld(float new[][COLS], float old[][COLS]);
+void CalculateNew(float new[][COLS], float old[][COLS], int xsource, int ysource);
+void PrintGrid(float grid[][COLS], int xsource, int ysource);
+
+
+int main(int argc, char * argv[]) {
+  // Pseudo code
+  // for each iteration {
+  //   CopyNewToOld(new, old);
+  //   CalculateNew(new, old, xsource, ysource);
+  //   Optionally, PrintGridtoFile(new, xsource, ysource);
+  // }
+  return 0;
+}
+
+// function implementations
+void CopyNewToOld(float new[][COLS], float old[][COLS]) {
+
+}
+
+void CalculateNew(float new[][COLS], float old[][COLS], int xsource, int ysource ) {
+
+}
+
+void PrintGrid(float grid[][COLS], int xsource, int ysource) {
+  
+}

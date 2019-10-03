5951bd861dbe6335c17df50ade96663033a65243
William B Messer <wmesser@clemson.edu>
2019-10-03 11:30:02 -0400

    MAKE GIT

--- main.c
+++ main.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+int ROWS = 1000
+int COLS = 1000;
+
 // function prototypes
 void CopyNewToOld(float new[][COLS], float old[][COLS]);
 void CalculateNew(float new[][COLS], float old[][COLS], int xsource, int ysource);
@@ -7,6 +10,12 @@ void PrintGrid(float grid[][COLS], int xsource, int ysource);
 
 
 int main(int argc, char * argv[]) {
+  if (argc != 3) {
+    printf("No size specified on command line. Using default %dx%d\n",ROWS,COLS);
+  } else {
+    ROWS = argv[1];
+    COLS = argv[2];
+  }
   // Pseudo code
   // for each iteration {
   //   CopyNewToOld(new, old);
@@ -26,5 +35,5 @@ void CalculateNew(float new[][COLS], float old[][COLS], int xsource, int ysource
 }
 
 void PrintGrid(float grid[][COLS], int xsource, int ysource) {
-  
+
 }
